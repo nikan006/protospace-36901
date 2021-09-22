6f3881f68bf2f6f8bc73972b689f81815387a824
岸拓成 <88487269+nikan006@users.noreply.github.com>
2021-09-22 20:25:45 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -44,10 +44,10 @@ ActiveRecord::Schema.define(version: 2021_09_22_025435) do
   end
 
   create_table "prototypes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title"
-    t.text "catch_copy"
-    t.text "concept"
-    t.bigint "user_id"
+    t.string "title", null: false
+    t.text "catch_copy", null: false
+    t.text "concept", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_prototypes_on_user_id"
